c2373dcc56fc2f4eb4332ff862d69993503467d4
Jens Thiele <karme@karme.de>
2012-08-08 16:10:54 +0200

    fix typo

--- Plugins/ViaRoutePlugin.h
+++ Plugins/ViaRoutePlugin.h
@@ -127,7 +127,7 @@ public:
             segmentPhantomNodes.targetPhantom = phantomNodeVector[i+1];
             rawRoute.segmentEndCoordinates.push_back(segmentPhantomNodes);
         }
-        if((routParameters.options.Find("alt") != "false") && (1 == rawRoute.segmentEndCoordinates.size())) {
+        if((routeParameters.options.Find("alt") != "false") && (1 == rawRoute.segmentEndCoordinates.size())) {
 	    INFO("Checking for alternative paths");
             searchEngine->alternativePaths(rawRoute.segmentEndCoordinates[0],  rawRoute);
 
