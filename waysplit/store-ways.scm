3d63ea6e0a5163f33c2ef2f8b0aa990a7a94ff7b
Jens Thiele <karme@karme.de>
2013-05-21 09:42:02 +0200

    allow to switch db backend

--- waysplit/store-ways.scm
+++ waysplit/store-ways.scm
@@ -1,7 +1,15 @@
 #| -*- mode: scheme; coding: utf-8; -*- |#
-:; exec gosh -I. -- $0 "$@"
+:; exec gosh -Ftokyocabinet -I. -- $0 "$@"
 
-(use dbm.gdbm)
+(use dbm)
+(cond-expand
+ (tokyocabinet
+  (use dbm.tokyocabinet)
+  (define *dbclass* <tcbdb>))
+ (else
+  (use dbm.gdbm)
+  (define *dbclass* <gdbm>)))
+
 (use sxml.adaptor) ;; for assert
 (use sxml.sxpath)
 (use sxml.tools)
@@ -11,22 +19,24 @@
 (define way-tags (compose (cute alist->hash-table <> 'equal?)
                           (cute map (lambda(tag) (cons (sxml:attr tag 'k) (sxml:attr tag 'v))) <>)
                           (sxpath '(tag))))
-  
+
 (define (main args)
   (let-optionals* (cdr args) ((db-file "ways.dbm"))
-    (let ((db (dbm-open <gdbm> :path db-file :rw-mode :write)))
-      (until (read) eof-object? => expr
-             (case (car expr)
-               [(way)
-                (let1 tags (way-tags expr)
-                  (dbm-put! db (way-id expr)
-                            (write-to-string `((geom . ,(map (lambda(p)
-                                                               (map string->number (string-split p ",")))
-                                                             (string-split (ref tags "geometry") " ")))
-                                               (speed . ,(map (lambda(profile)
-                                                                (cons profile
-                                                                      (cons (string->number (ref tags #`"osrm:,|profile|:fwd:speed" "0"))
-                                                                            (string->number (ref tags #`"osrm:,|profile|:bwd:speed" "0")))))
-                                                              '(foot bicycle)))))))]))))
+    (let ((db (dbm-open *dbclass* :path db-file :rw-mode :write)))
+      (unwind-protect
+       (until (read) eof-object? => expr
+              (case (car expr)
+                [(way)
+                 (let1 tags (way-tags expr)
+                   (dbm-put! db (way-id expr)
+                             (write-to-string `((geom . ,(map (lambda(p)
+                                                                (map string->number (string-split p ",")))
+                                                              (string-split (ref tags "geometry") " ")))
+                                                (speed . ,(map (lambda(profile)
+                                                                 (cons profile
+                                                                       (cons (string->number (ref tags #`"osrm:,|profile|:fwd:speed" "0"))
+                                                                             (string->number (ref tags #`"osrm:,|profile|:bwd:speed" "0")))))
+                                                               '(foot bicycle)))))))]))
+       (dbm-close db))))
   0)
 
