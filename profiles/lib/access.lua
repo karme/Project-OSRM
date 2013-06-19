36b23bd14b46f3d611d4925b1780b293e260a855
Jens Thiele <karme@karme.de>
2013-06-19 16:26:51 +0200

    tag might be nil?!

--- profiles/lib/access.lua
+++ profiles/lib/access.lua
@@ -5,7 +5,7 @@ module "Access"
 function find_access_tag(source,access_tags_hierachy)
     for i,v in ipairs(access_tags_hierachy) do 
         local tag = source.tags:Find(v)
-        if tag ~= '' then
+        if tag and tag ~= '' then
             return tag
         end
     end
